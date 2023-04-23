3e002108e2024dd5505edf85d9ad0200117956d3
Tony <tong.decula@gmail.com>
2023-04-22 21:49:18 -0400

    feat sort and where

--- SwiftUI_Firebase/Core/Product/ProductView.swift
+++ SwiftUI_Firebase/Core/Product/ProductView.swift
@@ -11,9 +11,66 @@ import SwiftUI
 final class ProductViewModel: ObservableObject {
     
     @Published private(set) var products: [Product] = []
+    @Published private(set) var selectedFilterOption: FilterOption = .noFilter
+    @Published private(set) var selectedCategoryOption: CategoryOption = .noCategory
     
-    func getAllProducts() async throws {
-        products = try await ProductsManager.shared.getAllProducts()
+//    func getAllProducts() async throws {
+//        products = try await ProductsManager.shared.getAllProducts(descending: selectedFilterOption?.priceDescending, category: selectedCategoryOption?.rawValue)
+//    }
+    
+    func filterSelected(option: FilterOption) async throws {
+        selectedFilterOption = option
+        getProducts()
+    }
+    
+    func categorySelected(option: CategoryOption) async throws {
+        selectedCategoryOption = option
+        getProducts()
+        
+    }
+    
+    func getProducts() {
+        Task {
+            products = try await ProductsManager.shared.getAllProducts(descending: selectedFilterOption.priceDescending, category: selectedCategoryOption.rawValue)
+        }
+    }
+    
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case priceHigh
+        case priceLow
+        
+        var priceDescending: Bool? {
+            switch self {
+            case .noFilter: return nil
+            case .priceHigh: return true
+            case .priceLow: return false
+            }
+        }
+        
+        var filterValue: String {
+            switch self {
+            case .noFilter: return "None"
+            case .priceHigh: return "Price High"
+            case .priceLow: return "Price Low"
+            }
+        }
+    }
+    
+    enum CategoryOption: String, CaseIterable {
+        case noCategory
+        case smartphones
+        case laptops
+        case fragrances
+        
+        var categoryValue: String {
+            switch self {
+            case .noCategory: return "None"
+            case .smartphones: return "Smart Phones"
+            case .laptops: return "Laptops"
+            case .fragrances: return "Fragrances"
+            }
+        }
     }
 }
 
@@ -28,9 +85,33 @@ struct ProductView: View {
             }
         }
         .navigationTitle("Products")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Menu("Filter: \(vm.selectedFilterOption.filterValue)") {
+                    ForEach(ProductViewModel.FilterOption.allCases, id: \.self) { option in
+                        Button(option.filterValue) {
+                            Task {
+                                try? await vm.filterSelected(option:option)
+                            }
+                        }
+                    }
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Menu("Category: \(vm.selectedCategoryOption.categoryValue)") {
+                    ForEach(ProductViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.categoryValue) {
+                            Task {
+                                try? await vm.categorySelected(option:option)
+                            }
+                        }
+                    }
+                }
+            }
+        }
         .onAppear {
             Task {
-                try? await vm.getAllProducts()
+                vm.getProducts()
             }
         }
     }
@@ -38,6 +119,8 @@ struct ProductView: View {
 
 struct ProductView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductView()
+        NavigationView {
+            ProductView()
+        }
     }
 }
--- SwiftUI_Firebase/Firestore/ProductsManager.swift
+++ SwiftUI_Firebase/Firestore/ProductsManager.swift
@@ -29,8 +29,38 @@ final class ProductsManager {
         try await productDocument(productId).getDocument(as: Product.self)
     }
     
-    func getAllProducts() async throws -> [Product] {
+    private func getAllProducts() async throws -> [Product] {
         try await productCollection.getDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsSortedByPrice(descending: Bool) async throws -> [Product] {
+        try await productCollection
+            .order(by: Product.CodingKeys.price.rawValue, descending: descending)
+            .getDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsForCategory(_ category: String) async throws -> [Product] {
+        try await productCollection
+            .whereField(Product.CodingKeys.category.rawValue, isEqualTo: category)
+            .getDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsByPrice(descending: Bool, category: String) async throws -> [Product] {
+        try await productCollection
+            .whereField(Product.CodingKeys.category.rawValue, isEqualTo: category)
+            .order(by: Product.CodingKeys.price.rawValue, descending: descending)
+            .getDocuments(as: Product.self)
+    }
+    
+    func getAllProducts(descending: Bool?, category: String?) async throws -> [Product] {
+        if let descending, let category {
+            return try await getAllProductsByPrice(descending: descending, category: category)
+        } else if let descending {
+            return try await getAllProductsSortedByPrice(descending: descending)
+        } else if let category{
+            return try await getAllProductsForCategory(category)
+        }
+        return try await getAllProducts()
     }
 }
 
